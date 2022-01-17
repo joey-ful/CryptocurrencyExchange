fae10f6fe0ae189cb3b3329ff3ce39e36767a0f5
joey-ful <isjoeyful@gmail.com>
2022-01-17 15:16:39 +0900

    feat: WebSocketTransaction 모델 구현

---
+++ CryptocurrencyTradingApp/Model/WebSocketTransaction.swift
@@ -0,0 +1,36 @@
+//
+//  WebSocketTransaction.swift
+//  CryptocurrencyTradingApp
+//
+//  Created by 홍정아 on 2022/01/17.
+//
+
+import Foundation
+
+struct WebSocketTransaction: Decodable {
+    let content: Content
+    
+    struct Content: Decodable {
+        let list: [Transaction]
+
+        struct Transaction: Decodable {
+            let symbol: String
+            let type: String
+            let price: String
+            let quantity: String
+            let amount: String
+            let dateTime: String
+            let upDown: String
+        
+            enum CodingKeys: String, CodingKey {
+                case symbol
+                case type = "buySellGb"
+                case price = "contPrice"
+                case quantity = "contQty"
+                case amount = "contAmt"
+                case dateTime = "contDtm"
+                case upDown = "updn"
+            }
+        }
+    }
+}
