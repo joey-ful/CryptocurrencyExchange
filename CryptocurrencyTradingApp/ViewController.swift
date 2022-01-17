c173190ccdda6775e0d45b9fdf15c0e710fc748c
joey-ful <isjoeyful@gmail.com>
2022-01-17 15:09:54 +0900

    Initial Commit

---
+++ CryptocurrencyTradingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptocurrencyTradingApp
+//
+//  Created by 홍정아 on 2022/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
