fc5540b70090cc41567fd9b018bd62082cedf0f3
jgkim1008 <rlawnsrjs100@naver.com>
2022-01-21 19:45:07 +0900

    feat: CandleStickAPI 통신 및 데이터를 적용하는 fetchData(notification:) 구현

--- CryptocurrencyTradingApp/ChartViewController.swift
+++ CryptocurrencyTradingApp/ChartViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ChartViewController: UIViewController {
+    
+    private var restAPIManager = RestAPIManager()
+    private var candleStickData: [[CandleStick.CandleData]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        NotificationCenter.default.addObserver(self, selector: #selector(fetchData), name: .candlestickNotification, object: nil)
+        restAPIManager.fetch(type: .candlestick, paymentCurrency: .KRW, coin: .ada, chartIntervals: .oneMinute)
     }
     
+    
+    @objc func fetchData(notification: Notification) {
+        if let result = notification.userInfo?["data"] as? [[CandleStick.CandleData]] {
+            candleStickData = result
+        } else {
+            print("error")
+        }
+    }
 }
