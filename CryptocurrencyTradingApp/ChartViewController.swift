b234dd99299b04094d50367ef3c43b65a55cc536
jgkim1008 <rlawnsrjs100@naver.com>
2022-01-21 19:34:33 +0900

    feat: chart를 보여줄 ChartViewController 생성

---
+++ CryptocurrencyTradingApp/ChartViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChartViewController.swift
+//  CryptocurrencyTradingApp
+//
+//  Created by 김준건 on 2022/01/21.
+//
+
+import UIKit
+
+class ChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
