f9260102ef9203c21bf5a81e6f0113e30ade975a
Joey Hong <isjoeyful@gmail.com>
2022-01-21 22:18:31 +0900

    feat: CoreDataStack 구현
    - persistentContainer 정의
    - NSManagedObjectContext 정의
    - saveContext() 정의

---
+++ CryptocurrencyTradingApp/CoreData/CoreDataStack.swift
@@ -0,0 +1,39 @@
+//
+//  CoreDataStack.swift
+//  CryptocurrencyTradingApp
+//
+//  Created by 홍정아 on 2022/01/21.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    private let modelName: String
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: modelName)
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    lazy var context: NSManagedObjectContext = persistentContainer.viewContext
+
+    func saveContext() {
+        guard context.hasChanges else { return }
+        do {
+            try context.save()
+        } catch {
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+}
