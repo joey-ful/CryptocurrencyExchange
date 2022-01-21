6aa17445aaa5b4f2b0528fbf732c1c7e957f6869
jgkim1008 <rlawnsrjs100@naver.com>
2022-01-21 19:50:32 +0900

    feat: Chart에 데이터를 생성하는 메서드 구현
    - CandleStickAPI의 경우 키가 없이 배열에 담겨져 데이터가 들어오기에 convert 메서드를 통해 타입을 변환

--- CryptocurrencyTradingApp/ChartViewController.swift
+++ CryptocurrencyTradingApp/ChartViewController.swift
@@ -6,11 +6,17 @@
 //
 
 import UIKit
+import SnapKit
+import Charts
 
 class ChartViewController: UIViewController {
     
+    private var chartView: CombinedChartView!
+
+    
     private var restAPIManager = RestAPIManager()
     private var candleStickData: [[CandleStick.CandleData]] = []
+    private var data: CombinedChartData = CombinedChartData()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,5 +32,38 @@ class ChartViewController: UIViewController {
         } else {
             print("error")
         }
+    }
+    
+    private func convert(_ candleData: CandleStick.CandleData) -> Double {
+        switch candleData {
+        case .string(let result):
+            return Double(result) ?? .zero
+        case .integer(let date):
+            return Double(date)
+        }
+    }
+    
+    private func makeLineChartData() -> LineChartDataSet {
+        let lineChartDataEntires = (.zero..<candleStickData.count - 1).map {(i) -> ChartDataEntry in
+            let data = convert(candleStickData[i][3])
+            let highValue = convert(candleStickData[i][2])
+            let date = convert(candleStickData[i][.zero])
+            return ChartDataEntry(x: date, y: highValue)
+        }
+        return LineChartDataSet(entries: lineChartDataEntires, label: "LineChart")
+    }
+    
+    private func makeCandleChartData() -> CandleChartDataSet {
+        let dataEntries1 = (.zero...candleStickData.count).map {(i) -> CandleChartDataEntry in
+            let val = Double(arc4random_uniform(40) + 10)
+            let high = Double(arc4random_uniform(9) + 8)
+            let low = Double(arc4random_uniform(9) + 8)
+            let open = Double(arc4random_uniform(6) + 1)
+            let close = Double(arc4random_uniform(6) + 1)
+            let even = arc4random_uniform(2) % 2 == 0
+            
+            return CandleChartDataEntry(x: Double(i), shadowH: val + high, shadowL: val-low, open: even ? val + open: val-open, close: even ? val-close: val + close)
+        }
+        return CandleChartDataSet(entries: dataEntries1, label: "candleChart")
     }
 }
