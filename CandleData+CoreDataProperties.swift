291f34b9242cf3a75d99d24e57ac8b931f08cb4b
Joey Hong <isjoeyful@gmail.com>
2022-01-21 22:17:19 +0900

    feat: CoreData Entity 생성
    - CandleData Manual/None으로 생성

---
+++ CandleData+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  CandleData+CoreDataProperties.swift
+//  CryptocurrencyTradingApp
+//
+//  Created by 홍정아 on 2022/01/21.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension CandleData {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CandleData> {
+        return NSFetchRequest<CandleData>(entityName: "CandleData")
+    }
+
+    @NSManaged public var date: Int64
+    @NSManaged public var openPrice: String?
+    @NSManaged public var closePrice: String?
+    @NSManaged public var highPrice: String?
+    @NSManaged public var lowPrice: String?
+    @NSManaged public var tradeVolume: String?
+
+}
+
+extension CandleData : Identifiable {
+
+}
