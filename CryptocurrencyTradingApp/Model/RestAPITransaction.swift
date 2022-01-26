56f7bf507db82138883ffc13ac0b840d503500cf
Joey Hong <isjoeyful@gmail.com>
2022-01-26 15:23:17 +0900

    feat: Transaction 타입들 구현
    - RestAPI 디코딩에 필요한 RestAPITransaction 타입 구현
    - RestAPI와 WebSocket 데이터 형식이 달라 실사용할 Transaction 타입 구현

---
+++ CryptocurrencyTradingApp/Model/RestAPITransaction.swift
@@ -0,0 +1,27 @@
+//
+//  TransactionHistory.swift
+//  CryptocurrencyTradingApp
+//
+//  Created by 홍정아 on 2022/01/25.
+//
+
+import Foundation
+
+struct RestAPITransaction: Decodable {
+    let data: [Transaction]
+    
+    struct Transaction: Decodable {
+        let date: String
+        let type: String
+        let quantity: String
+        let price: String
+        let amount: String
+        
+        enum CodingKeys: String, CodingKey {
+            case type, price
+            case date = "transaction_date"
+            case quantity = "units_traded"
+            case amount = "total"
+        }
+    }
+}
