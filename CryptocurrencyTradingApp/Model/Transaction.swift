647a75b1a1a7cf7b052355a806984057366e5550
Joey Hong <isjoeyful@gmail.com>
2022-01-28 00:49:55 +0900

    feat: DayTransactionViewModel 구현
    - 체결내역의 일별 테이블뷰 셀을 담당할 DayTransactionViewModel 타입 구현
    - CandleData를 받아 가공하는 로직을 CandleCoreDataManager의 로직을 임시로 가져와 사용
    - 체결내역 시간별/일별 화면에서 모두 사용할 수 있도록 Transaction 타입에 프로퍼티 추가 
      및 일부 프로퍼티를 옵셔널로 변경

--- CryptocurrencyTradingApp/Model/Transaction.swift
+++ CryptocurrencyTradingApp/Model/Transaction.swift
@@ -10,18 +10,36 @@ import Foundation
 struct Transaction: Hashable {
     let id = UUID()
     let symbol: String?
-    let type: String
-    let price: String
-    let quantity: String
-    let amount: String
+    let type: String?
+    var price: String
+    let prevPrice: String?
+    var quantity: String?
+    var amount: String?
     let date: String
     let upDown: String?
     
+    init(symbol: String? = nil, type: String? = nil, price: String, prevPrice: String? = nil, quantity: String? = nil, amount: String? = nil, date: String, upDown: String? = nil) {
+        self.symbol = symbol
+        self.type = type
+        self.price = price
+        self.prevPrice = prevPrice
+        self.quantity = quantity
+        self.amount = amount
+        self.date = date
+        self.upDown = upDown
+    }
+    
     static func == (lhs: Transaction, rhs: Transaction) -> Bool {
         return lhs.id == rhs.id
+        && lhs.price == rhs.price
+        && lhs.quantity == rhs.quantity
+        && lhs.amount == rhs.amount
     }
     
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
+        hasher.combine(price)
+        hasher.combine(quantity)
+        hasher.combine(amount)
     }
 }
---
+++ CryptocurrencyTradingApp/Protocol/TransactionViewModelType.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  CryptocurrencyTradingApp
+//
+//  Created by 홍정아 on 2022/01/27.
+//
+
+import Foundation
+
+protocol TransactionViewModelType {}
