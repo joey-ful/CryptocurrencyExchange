1eb44919569e215639c40fb46a750fefc71a41ee
jgkim1008 <rlawnsrjs100@naver.com>
2022-02-06 21:34:52 +0900

    refactor: Chart 라이브러리 추가
    * 기존 SwiftUI로 Chart를 그리던 로직은 Chart 라이브러리로 구현하기 위해 Chart 라이브러리 추가
    * CandleStickChartView 생성 및 오토레이아웃 구현

--- CryptocurrencyTradingApp/Controller/DetailChartViewController.swift
+++ CryptocurrencyTradingApp/Controller/DetailChartViewController.swift
@@ -7,37 +7,67 @@
 
 import UIKit
 import SnapKit
-import SwiftUI
+import Charts
 
 class DetailChartViewController: UIViewController {
-    private let coin: CoinType?
-    private var chartView: ChartView?
-    
-    private lazy var hostingController: UIHostingController<ChartView?> = {
-        let controller = UIHostingController(rootView: chartView)
-        controller.view.translatesAutoresizingMaskIntoConstraints = false
-        return controller
+    private lazy var chartView: CandleStickChartView = {
+        var chartView = CandleStickChartView()
+        chartView.rightAxis.setLabelCount(5, force: true)
+        chartView.leftAxis.enabled = false
+        chartView.legend.enabled = false
+        chartView.leftAxis.enabled = false
+        chartView.xAxis.labelPosition = .bottom
+        return chartView
     }()
     
     private lazy var timeControl: UISegmentedControl = {
-        let items = ["1분","10분","30분","1시간","일"]
-        let timeControl = UISegmentedControl(items: items)
-        timeControl.selectedSegmentIndex = 0
-        timeControl.layer.borderColor = UIColor.gray.cgColor
-        timeControl.addTarget(self, action: #selector(menuSelect), for: .valueChanged)
-        timeControl.layer.masksToBounds = true
-        return timeControl
-    }()
+            let items = ["1분","10분","30분","1시간","일"]
+            let timeControl = UISegmentedControl(items: items)
+            timeControl.selectedSegmentIndex = 0
+            timeControl.layer.borderColor = UIColor.gray.cgColor
+            timeControl.addTarget(self, action: #selector(menuSelect), for: .valueChanged)
+            timeControl.layer.masksToBounds = true
+            return timeControl
+        }()
+    
+    
+    
+    private var restAPIManager = RestAPIManager()
+    private var candleStickData: [[CandleStick.CandleStickData]] = []
+    private var data: CandleChartData = CandleChartData()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setLayout()
+        
+        view.backgroundColor = .white
+  
+        requestNetwork()
+        setLayoutForChartView()
     }
     
-    init(coin: CoinType) {
-        self.coin = coin
-        chartView = ChartView(coin: coin, chartIntervals: .oneMinute)
-        super.init(nibName: nil, bundle: nil)
+    @objc private func menuSelect(_ sender: UISegmentedControl) {
+//            switch sender.selectedSegmentIndex {
+//            case 0:
+//                chartView?.viewModel.initiateViewModel(coin: coin ?? .btc
+//                                                      , chartIntervals: .oneMinute)
+//            case 1:
+//                chartView?.viewModel.initiateViewModel(coin: coin ?? .btc
+//                                                      , chartIntervals: .tenMinute)
+//            case 2:
+//                chartView?.viewModel.initiateViewModel(coin: coin ?? .btc
+//                                                      , chartIntervals: .thirtyMinute)
+//            case 3:
+//                chartView?.viewModel.initiateViewModel(coin: coin ?? .btc
+//                                                      , chartIntervals: .oneHour)
+//            case 4:
+//                chartView?.viewModel.initiateViewModel(coin: coin ?? .btc
+//                                                      , chartIntervals: .twentyFourHour)
+//            default:
+//                chartView?.viewModel.initiateViewModel(coin: coin ?? .btc
+//                                                      , chartIntervals: .twentyFourHour)
+//            }
+        }
     }
     
     required init?(coder: NSCoder) {
@@ -67,19 +97,24 @@ class DetailChartViewController: UIViewController {
         }
     }
     
-    private func setLayout() {
-        addChild(hostingController)
+    private func setLayoutForChartView() {
         view.addSubview(timeControl)
-        view.addSubview(hostingController.view)
+        view.addSubview(chartView)
         
         timeControl.snp.makeConstraints { make in
                 make.leading.equalToSuperview()
                 make.trailing.equalToSuperview()
-                make.top.equalToSuperview()
-                make.bottom.equalTo(hostingController.view.snp.top)
-                make.width.equalToSuperview()
+                make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
                 make.height.equalToSuperview().multipliedBy(0.03)
             }
+        
+        chartView.snp.makeConstraints { make in
+            make.leading.equalTo(view.safeAreaLayoutGuide)
+            make.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(timeControl.snp.bottom)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
             
             hostingController.didMove(toParent: self)
             hostingController.view.snp.makeConstraints { make in
