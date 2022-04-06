f0f55a855c5c032d49c7eb882e95c4b525ada89e
jgkim1008 <rlawnsrjs100@naver.com>
2022-04-06 14:47:51 +0900

    refactor: AssetStatus를 RxSwift로 리팩토링
    * 불필요한 AssetStatusViewModel 삭제
    * UI에 적용하기 위하여 ViewModel의 데이터를 BehaviorRelay로 구현

--- CryptocurrencyTradingApp/Controller/AssetStatusViewController.swift
+++ CryptocurrencyTradingApp/Controller/AssetStatusViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 import SnapKit
-
-//typealias StatusDataSource = UITableViewDiffableDataSource<Int, AssetStatus>
+import RxSwift
+import RxCocoa
+import RxRelay
 
 class AssetStatusViewController: UIViewController {
     private let viewModel: AssetStatusListViewModel
     private let tableView = UITableView(frame: .zero, style: .grouped)
     private let searchBar = UISearchBar(frame: .zero)
+    var disposeBag = DisposeBag()
     
     init(viewModel: AssetStatusListViewModel) {
         self.viewModel = viewModel
@@ -29,23 +31,19 @@ class AssetStatusViewController: UIViewController {
         buildUI()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        viewModel.makeSnapshot()
-    }
 }
 
 extension AssetStatusViewController {
-
+    
     private func buildUI() {
         view.backgroundColor = .white
         buildSearchBar()
-        buildSelection()
+        searchResultUpdating()
         buildTableView()
     }
-
+    
     private func buildSearchBar() {
         view.addSubview(searchBar)
-        searchBar.delegate = self
         searchBar.placeholder = "코인명 또는 심볼 검색"
         searchBar.searchTextField.font = .preferredFont(forTextStyle: .subheadline)
         searchBar.searchTextField.backgroundColor = .white
@@ -63,17 +61,13 @@ extension AssetStatusViewController {
         searchBar.layer.cornerRadius = 4
     }
     
-    private func buildSelection() {
-        
-    }
-}
+    private func searchResultUpdating() {
+        _ = searchBar.rx.text.orEmpty
+            .distinctUntilChanged()
+            .subscribe(onNext: {
+                self.viewModel.searchResult(query: $0)
+            })
 
-// MARK: SearchResultsUpdating
-extension AssetStatusViewController: UISearchBarDelegate {
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        let text = searchBar.text
-        viewModel.filter(text)
-        viewModel.makeSnapshot()
     }
 }
 
@@ -82,15 +76,23 @@ extension AssetStatusViewController {
     private func buildTableView() {
         setUpTableView()
         setTableViewAutoLayout()
-        registerCell()
     }
-
+    
     private func setUpTableView() {
         tableView.delegate = self
         tableView.register(StatusCell.self, forCellReuseIdentifier: "statusCell")
         tableView.register(StatusHeader.self, forHeaderFooterViewReuseIdentifier: "statusHeader")
+        
+        viewModel.filterdObservable
+            .asDriver(onErrorJustReturn: [])
+            .drive(tableView.rx.items(cellIdentifier: "statusCell", cellType: StatusCell.self)) {
+                index, item, cell in
+                cell.configure(with: item)
+            }
+            .disposed(by: disposeBag)
     }
-
+    
+    
     private func setTableViewAutoLayout() {
         view.addSubview(tableView)
         tableView.backgroundColor = .white
@@ -101,43 +103,26 @@ extension AssetStatusViewController {
             make.trailing.equalToSuperview()
             make.bottom.equalToSuperview()
         }
-
+        
         tableView.separatorInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
         tableView.estimatedRowHeight = UIFont.preferredFont(forTextStyle: .subheadline).pointSize
         + UIFont.preferredFont(forTextStyle: .caption1).pointSize
         + 30
     }
-
-    private func registerCell() {
-        viewModel.dataSource = StatusDataSource(tableView: tableView,
-                                        cellProvider: { [weak self] tableView, indexPath, mainListCoin in
-
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "statusCell",
-                                                           for: indexPath) as? StatusCell,
-                  let viewModel = self?.viewModel.assetStatusViewModel(at: indexPath.row) else {
-                      return UITableViewCell()
-            }
-
-            cell.configure(viewModel: viewModel)
-
-            return cell
-        })
-        tableView.dataSource = viewModel.dataSource
-    }
 }
 
 // MARK: TableViewHeader
 extension AssetStatusViewController: UITableViewDelegate {
-
+    
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         guard let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "statusHeader")
                 as? StatusHeader
         else {
             return UIView()
         }
-
+        
         header.configure()
-
+        
         return header
     }
 }
